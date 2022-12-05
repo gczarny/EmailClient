b2845c461755a66bef23709226e695225a0de627
gczarny <grzegorz.czarny93@gmail.com>
2022-12-05 19:22:32 +0100

    Set initializing stages, delete controller from fxml file

--- untitled/src/com/app/view/ViewFactory.java
+++ untitled/src/com/app/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.app.view;
 
 import com.app.EmailManager;
+import com.app.controller.BaseController;
+import com.app.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -12,6 +20,21 @@ public class ViewFactory {
 
     public void showLoginWindow(){
         System.out.println("Show login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try{
+            parent = fxmlLoader.load();
+        } catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
