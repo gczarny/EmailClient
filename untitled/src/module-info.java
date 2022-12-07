ad9653aab2b430df0d65c9b1c1685659468f4775
gczarny <grzegorz.czarny93@gmail.com>
2022-12-07 18:50:13 +0100

    Add java mail and activation

--- untitled/src/module-info.java
+++ untitled/src/module-info.java
@@ -1,9 +1,11 @@
-module FirstJavaFX {
+module EmailClient {
 
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
+    requires activation;
+    requires java.mail;
 
     opens com.app;
     opens com.app.view;
