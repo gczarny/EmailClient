b50a778cdad155fe3e550a386e6691db02cb29a6
gczarny <grzegorz.czarny93@gmail.com>
2022-12-09 20:01:07 +0100

    Add messages after login from services

--- untitled/src/com/app/controller/services/FetchFolderService.java
+++ untitled/src/com/app/controller/services/FetchFolderService.java
@@ -39,10 +39,34 @@ public class FetchFolderService extends Service<Void> {
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
+            FetchMessagesFolder(folder, emailTreeItem);
             if(folder.getType() == Folder.HOLDS_FOLDERS){
                 Folder[] subFolders = folder.list();
                 handleFolders(subFolders, emailTreeItem);//recursion, call same method
             }
         }
+    }
+
+    private void FetchMessagesFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
+        //for each folder create separate service
+        Service fetchMessagesService = new Service() {
+            @Override
+            protected Task createTask() {
+                return new Task() {
+                    @Override
+                    protected Object call() throws Exception {
+                        if(folder.getType() != Folder.HOLDS_FOLDERS){
+                            folder.open(Folder.READ_WRITE);
+                            int folderSize = folder.getMessageCount();
+                            for(int i = folderSize; i > 0; i--){
+                                System.out.println(folder.getMessage(i).getSubject());
+                            }
+                        }
+                        return null;
+                    }
+                };
+            }
+        };
+        fetchMessagesService.start();
     }
 }
